2057a7be2039d7c538e0f3f6b74d2a3cb7a2019a
Karamoldoev Emir <karamoldoevee@gmail.com>
2019-11-16 13:37:42 +0600

    Добавлено представление смены пароля и информации профиля

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -79,6 +79,7 @@ def register_view(request, *args, **kwargs):
 
     return render(request, 'user_create.html', context={'form': form})
 
+
 class UserDetailView(DetailView):
 
     model = User
@@ -86,3 +87,35 @@ class UserDetailView(DetailView):
     template_name = 'user_detail.html'
 
     context_object_name = 'user_obj'
+
+
+class UserPersonalInfoChangeView(UserPassesTestMixin, UpdateView):
+    model = User
+
+    template_name = 'user_info_change.html'
+
+    form_class = UserChangeForm
+
+    context_object_name = 'user_obj'
+
+    def test_func(self):
+        return self.request.user.pk == self.kwargs['pk']
+
+    def get_success_url(self):
+        return reverse('accounts:detail', kwargs={'pk': self.object.pk})
+
+
+class UserPasswordChangeView(UserPassesTestMixin, UpdateView):
+    model = User
+
+    template_name = 'user_password_change.html'
+
+    form_class = PasswordChangeForm
+
+    context_object_name = 'user_obj'
+
+    def test_func(self):
+        return self.request.user.pk == self.kwargs['pk']
+
+    def get_success_url(self):
+        return reverse('accounts:login')
