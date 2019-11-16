071a6bd224755cbf53ba248251410bf61d4ade11
Karamoldoev Emir <karamoldoevee@gmail.com>
2019-11-16 15:07:21 +0600

    Создана модель Товара и Отзыва, зарегана в админке и данные добавлены в фикстуру

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from webapp.models import Product, Review
 
-# Register your models here.
+class ReviewtAdmin(admin.TabularInline):
+    model = Review
+    fields = ['author', 'text', 'raiting']
+    extra = 0
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'name', 'category', 'description']
+    # list_filter = ['name', 'category']
+    list_display_links = ['pk', 'name']
+    search_fields = ['name', 'category']
+    exclude = []
+    inlines = [ReviewtAdmin]
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Review)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,23 +1,29 @@
-# from django.conf import settings
-# from django.db import models
-#
-#
-#
-# class Product(models.Model):
-#     SMARTPHONE = 'SM'
-#     LAPTOP = 'LP'
-#     EARPHONE = 'ER'
-#     CATEGORY_CHOICES = [
-#         ('SM', 'Smartphone'),
-#         ('LP', 'Laptop'),
-#         ('ER', 'Earphone'),
-#     ]
-#     name = models.CharField(max_length=100, blank=False, verbose_name='Название'),
-#     category = models.CharField(max_length=10, choices=CATEGORY_CHOICES, default=SMARTPHONE, verbose_name='Категория')
-#     description = models.TextField(max_length=2000, blank=True, verbose_name='Описание')
-#     image = models.ImageField(upload_to='product_images', null=True, blank=True, verbose_name='Картинка')
-#
-# class Review(models.Model):
-#     author
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db import models
+from django.contrib.auth.models import User
+
+
+
+class Product(models.Model):
+    SMARTPHONE = 'SM'
+    LAPTOP = 'LP'
+    EARPHONE = 'ER'
+    CATEGORY_CHOICES = [
+        ('SM', 'Smartphone'),
+        ('LP', 'Laptop'),
+        ('ER', 'Earphone'),
+    ]
+    name = models.CharField(max_length=100, blank=False, verbose_name='Название')
+    category = models.CharField(max_length=10, choices=CATEGORY_CHOICES, default=SMARTPHONE, verbose_name='Категория')
+    description = models.TextField(max_length=2000, blank=True, verbose_name='Описание')
+    image = models.ImageField(upload_to='product_images', null=True, blank=True, verbose_name='Картинка')
+
+class Review(models.Model):
+    author = models.ForeignKey(User, null=True, blank=True, default=None, verbose_name='Автор', on_delete=models.CASCADE,
+                               related_name='review')
+    product = models.ForeignKey('webapp.Product', related_name='review', on_delete=models.CASCADE, verbose_name='Продукт')
+
+    text = models.TextField(max_length=400, verbose_name='Текст отзыва')
+    raiting = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)], null=False, blank=False)
 
 
